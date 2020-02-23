1fd5c4ddfd1b8074cf8f52a03e81b5542bfe5c45
Gabr1el94 <bielsystem64@gmail.com>
2020-02-22 23:33:48 -0300

    Service transacional as classes

---
+++ src/main/java/com/gabrielsoaresdev/ecommerce/Ecommerce/service/ProdutoService.java
@@ -0,0 +1,10 @@
+package com.gabrielsoaresdev.ecommerce.Ecommerce.service;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
+public class ProdutoService {
+
+}
